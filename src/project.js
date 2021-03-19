e73f527f50d9e88f82d564e882772d9156b29a6a
Emewjin <yujinwork03@gmail.com>
2021-03-19 11:37:36 +0900

    update project.js

--- src/project.js
+++ src/project.js
@@ -1,6 +1,6 @@
 const filterIcons = document.querySelectorAll(".tools-img img");
-const projectItem = document.querySelectorAll(".project div");
-const OPACITY = "opacity-half"
+const projectItem = document.querySelectorAll(".project-item");
+const items = projectItem[0].childNodes;
 const SCALE = "click-scale"
 
 function changeClassList(a,b,c){
@@ -11,17 +11,20 @@ function changeClassList(a,b,c){
 
 function showJsProject(){
     changeClassList(0,1,2);
-    projectItem[0].classList.add(OPACITY);
+    items[1].classList.add(HIDDEN);
+    items[3].classList.add(HIDDEN);
 }
 
 function showHtmlProject(){
     changeClassList(1,0,2);
-    projectItem[0].classList.remove(OPACITY);
+    items[1].classList.remove(HIDDEN);
+    items[3].classList.remove(HIDDEN);
 }
 
 function showCssProject(){
     changeClassList(2,0,1);
-    projectItem[0].classList.remove(OPACITY);
+    items[1].classList.remove(HIDDEN);
+    items[3].classList.remove(HIDDEN);
 }
 
 filterIcons[0].addEventListener("click", showJsProject);
